959f4b0c2d4725b7d5056d5c734f96024a2bbb7b
zwj <2317133489@qq.com>
2019-08-26 11:26:23 +0800

    ok

---
+++ vue-test/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+ margin:0;
+ padding:0;
+ list-style: none;
+}
+html,body{
+  width:100%;
+  height:100%;
+}
+#app {
+width:100%;
+height:100%;
+}
+</style>
---
+++ vue-test/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div> -->
+  <div class="home">
+    这是home
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  created(){
+    console.log(document.body.clientWidth,document.body.clientTop);
+  }
+}
+</script>
+<style lang="scss">
+@media screen and (max-width:980px){
+.home{
+  width:100%;
+  height:20px;
+  background:pink;
+}
+}
+
+</style>
+
