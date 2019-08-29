da289f4e87bb2dfc96704014546c9220a4a3dfc1
zwj <2317133489@qq.com>
2019-08-29 21:34:34 +0800

    login

--- vue-test/src/store/modules/Login.js
+++ vue-test/src/store/modules/Login.js
@@ -1,26 +1,26 @@
 import { login,register} from "../../services/index";
 const state = {
-    list:[],
+    list:"",
     arr:[]
   };
   
   const mutations = {
     pushAdd(state, payload) {
-      state.list = payload;
+      state.list=payload;
     },zhuce(state,payload){
-      state.arr.push(payload);
+      state.arr=payload;
     }
   };
   const actions = {
     //添加试题
     async addData({ commit }, payload) {
       let data = await login(payload);
-      console.log("res",data);
-      commit("pushAdd", data);
+      console.log("res",data.data.code);
+      commit("pushAdd", data.data.code);
     },
     async regist({commit},payload){
       let data=await register(payload);
-      commit("zhuce",data.data.code);
+      commit("zhuce",data.data);
     }
   };
   
